4d1c48d248fd36663523bcf6e5516dc32527ae2e
AM021975 <ax655239652229@gmail.com>
2015-04-28 17:15:34 +0800

    A AM021975 陳翌睿提交

--- Question1.java
+++ Question1.java
@@ -1,6 +1,7 @@
 ﻿public class Question1{
 	public statis void main(String[] args){
 		System.out.println("A");
+		System.out.println("Finish");
 		System.out.println("Test");
 		System.out.println("B");
 	}
